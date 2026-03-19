e5ca3a2ba73ca7a19b30fa9da2c9742d1c737f50
arih-hue <arihantaydav2411@gmail.com>
2026-03-19 22:36:46 +0530

    Second commit

--- lib/pages/weather_page.dart
+++ lib/pages/weather_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/services/weather_service.dart';
+
+import '../models/weather_model.dart';
 
 class WeatherPage extends StatefulWidget {
   const WeatherPage({super.key});
@@ -8,8 +11,46 @@ class WeatherPage extends StatefulWidget {
 }
 
 class _WeatherPageState extends State<WeatherPage> {
+
+  //api key
+  final _weatherService = WeatherService('b825ab38a7f7e4834d33a6c323d54867');
+  Weather? _weather;    //weather object
+
+  //fetch weather
+  _fetchWeather() async     // method to fetch the weather
+      {
+    String cityName = await _weatherService.getCurrentCity();
+    // get weather for city
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    }
+    catch (e) {
+      print(e);
+    }
+  }
+
+  //weather amimations
+
+  @override
+  void initState(){
+    super.initState();
+    _fetchWeather();
+  }
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(_weather?.cityName ?? 'Loading City...'),     //city name
+
+            Text('${_weather?.temperature.round()}°C')     //temperature of the city
+        ],),
+      )
+    );
   }
 }
